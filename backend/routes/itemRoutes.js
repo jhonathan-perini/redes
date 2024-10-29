4f3a23f6a4d876141e91eabf80d9e49ca1de8c97
Jhonathan Perini <jperini@padtec.com.br>
2024-10-29 18:54:44 -0300

    teste

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { getAllItems, createItem, updateItem, deleteItem } from '../controllers/ItemController.js';
-import { getItem } from '../controllers/ItemController.js';
+import { getAllItems, createItem, updateItem, deleteItem } from '../controllers/itemController.js';
+import { getItem } from '../controllers/itemController.js';
 
 const router = express.Router();
 
