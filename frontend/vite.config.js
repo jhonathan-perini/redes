d84a2be46591a97fa09d0833e0c9c75a012e3966
Jhonathan Perini <jperini@padtec.com.br>
2024-10-29 23:04:23 -0300

    teste

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Escuta em todas as interfaces de rede
-    port: 3000, // Altere para a porta desejada
-  },
+  
 })
